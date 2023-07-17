e6bf432de83c63d9aed5be5a7f6401410ba41a3b
Samuel Crockford <crockfords22@icloud.com>
2023-07-17 14:56:36 -0400

    testing with own code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Form from "./Form";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentDidMount() {
+    const url = "https://jsonplaceholder.typicode.com/posts";
+    fetch(url)
+    .then(response => response.json())
+    .then(json => this.setState({ posts: json }))
+  }
+  
+
   render() {
+    const { posts } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello from Sam!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <div class="jumbotron">
+          <h1 class="display-4">Onset Weekend 2023</h1>
+          <small class="text-muted">Thursday, September 21 - Sunday, September 24 </small>
+        </div>    
+          <div className="App">
+            <div className="py-6">
+              <Form />
+            </div>
+        </div>
+        <div className="jumbotron">
+          Whos Going ?
+        </div>
+        {posts.map((post) => (
+          <div className="card" key={post.id}>
+            <div className="card-header">
+              #{post.id} {post.title}
+            </div>
+            <div className="card-body">
+              <p className="card-text">{post.body}</p>
+            </div>
+          </div>
+        ))}
       </div>
     );
   }
 }
-
 export default App;
---
+++ src/Form.js
@@ -0,0 +1,78 @@
+import useForm from "./UseForm";
+
+const FORM_ENDPOINT = "https://herotofu.com/start"; // TODO - update to the correct endpoint
+
+const Form = () => {
+  const additionalData = {
+    sent: new Date().toISOString(),
+  };
+
+  const { handleSubmit, status, message } = useForm({
+    additionalData,
+  });
+
+  if (status === "success") {
+    return (
+      <>
+        <div className="text-2xl">Thank you!</div>
+        <div className="text-md">{message}</div>
+      </>
+    );
+  }
+
+  if (status === "error") {
+    return (
+      <>
+        <div className="text-2xl">Something bad happened!</div>
+        <div className="text-md">{message}</div>
+      </>
+    );
+  }
+
+  return (
+    <form
+      action={FORM_ENDPOINT}
+      onSubmit={handleSubmit}
+      method="POST"
+    >
+      <div className="mb-3 pt-0">
+        <input
+          type="text"
+          placeholder="Your name"
+          name="name"
+          className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+          required
+        />
+      </div>
+      <div className="mb-3 pt-0">
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+          required
+        />
+      </div>
+      <div className="mb-3 pt-0">
+        <textarea
+          placeholder="Your message"
+          name="message"
+          className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+          required
+        />
+      </div>
+      {status !== "loading" && (
+        <div className="mb-3 pt-0">
+          <button
+            className="btn btn-dark"
+            type="submit"
+          >
+            RSVP
+          </button>
+        </div>
+      )}
+    </form>
+  );
+};
+
+export default Form;
