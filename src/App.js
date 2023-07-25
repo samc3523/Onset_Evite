27a6e71536de4acb91b7ddd29a040aadb612fad9
Samuel Crockford <crockfords22@icloud.com>
2023-07-25 17:10:11 -0400

    trying a little iframe thingy

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ class App extends Component {
         <div class="jumbotron">
           <h1 class="display-4">Onset Weekend 2023</h1>
           <small class="text-muted">Thursday, September 21 - Sunday, September 24 </small>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2977.0300690667273!2d-70.66737145082854!3d41.741444297372!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e4c5d1900e73fb%3A0x8607e79e92d00daf!2s19%20South%20Blvd%2C%20Wareham%2C%20MA%2002571!5e0!3m2!1sen!2sus!4v1690319309370!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>    
           <div className="App">
             <div className="py-6">
